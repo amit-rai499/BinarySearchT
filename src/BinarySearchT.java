d5d15aea2919b2f1bb95063ef7c6583837a9d06c
Amit Rai <amit.rai499@gmail.com>
2019-04-09 22:35:50 +0530

    Initial commit

---
+++ src/BinarySearchT.java
@@ -0,0 +1,56 @@
+class  Node
+{
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int data) {
+        this.data = data;
+        this.left=null;
+        this.right=null;
+    }
+}
+public class BinarySearchT {
+    public static Node insert(Node root,Node newnode)
+    {
+        if(root==null)
+        {
+            root=newnode;
+        }
+        if(root.data>newnode.data)
+        {
+            if(root.left==null)
+                root.left=newnode;
+            else
+                insert(root.left,newnode);
+        }
+        else if(root.data<newnode.data)
+        {
+            if(root.right==null)
+                root.right=newnode;
+            else
+                insert(root.right,newnode);
+        }
+        return root;
+    }
+    public static void inOrderDisplay(Node root)
+    {
+        if(root==null)
+            return;
+        inOrderDisplay(root.left);
+        System.out.print(root.data+" ");
+        inOrderDisplay(root.right);
+    }
+    public static void main(String[] args) {
+
+        Node Root=insert(null,new Node(17));
+        insert(Root,new Node(8));
+        insert(Root,new Node(56));
+        insert(Root,new Node(38));
+        insert(Root,new Node(45));
+        insert(Root,new Node(3));
+        insert(Root,new Node(18));
+        insert(Root,new Node(90));
+        inOrderDisplay(Root);
+    }
+}
